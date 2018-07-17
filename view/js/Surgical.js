1cd9a22356fb0b9902052fed47e884c86a8ffff4
c-beltran <carlosbeltran90@gmail.com>
2018-07-16 22:30:08 -0400

    started surgical supply page

---
+++ view/js/Surgical.js
@@ -0,0 +1,19 @@
+
+	// Initialize and add the map
+	function initMap() {
+	  // The location of Uluru
+	  var pharmacy = {lat: 40.705371, lng: -73.690163};
+	  // The map, centered at Uluru
+	  var map = new google.maps.Map(
+	  	document.getElementById('map'), {zoom: 17, center: pharmacy});
+	  // The marker, positioned at Uluru
+	  var marker = new google.maps.Marker({position: pharmacy, map: map});
+
+	  //automatic zoom
+	  // bounds = new google.maps.LatLngBounds();
+	  // var loc = new google.maps.LatLng(marker.position.lat(40.7054081), marker.position.lng(-73.6909824));
+	  // bounds.extend(loc);
+	  // map.fitBounds(bounds); //auto-zoom
+	  // map.setZoom(17);
+	  // map.panTo(bounds);
+	}
