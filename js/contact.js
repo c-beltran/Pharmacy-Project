c0395d27b1c9bc911786511fe66ced4447bc0a4f
c-beltran <carlosbeltran90@gmail.com>
2018-07-18 00:00:56 -0400

    finalized majority of site, need to do touch ups

---
+++ js/contact.js
@@ -0,0 +1,26 @@
+
+	// Initialize and add the map
+	function initMap() {
+	  // The location of 
+	  var pharmacy = {lat: 40.705371, lng: -73.690163};
+	  // The map, centered at 
+	  var map = new google.maps.Map(
+	  	document.getElementById('map'), {zoom: 17, center: pharmacy});
+	  // The marker, positioned at 
+	  var marker = new google.maps.Marker({position: pharmacy, map: map});
+
+
+	  //automatic zoom
+	  // bounds = new google.maps.LatLngBounds();
+	  // var loc = new google.maps.LatLng(marker.position.lat(40.7054081), marker.position.lng(-73.6909824));
+	  // bounds.extend(loc);
+	  // map.fitBounds(bounds); //auto-zoom
+	  // map.setZoom(17);
+	  // map.panTo(bounds);
+	}
+	//scroll effect
+	$("#logo").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#cc-logos").offset().top},
+        'slow');
+	});
